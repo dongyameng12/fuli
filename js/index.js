4a17d0b53b0fd8374166c3d6231cf24c73d24177
yangtao <1509440167@qq.com>
2019-05-22 10:51:36 +0800

    啛啛喳喳

--- js/index.js
+++ js/index.js
@@ -1,32 +1,58 @@
+// (function (doc, win) {
+//     var docEl = doc.documentElement,
+//         resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+//         recalc = function () {
+//             var clientWidth = docEl.clientWidth;
+//             if (!clientWidth) return;
+//             docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+//         };
+//     if (!doc.addEventListener) return;
+//     win.addEventListener(resizeEvt, recalc, false);
+//     doc.addEventListener('DOMContentLoaded', recalc, false);
+// })
+// (document, window);
 (function (doc, win) {
-    var docEl = doc.documentElement,
-        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-        recalc = function () {
-            var clientWidth = docEl.clientWidth;
-            if (!clientWidth) return;
-            docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
-        };
+    console.log("dpr:" + win.devicePixelRatio);
+    var docEle = doc.documentElement,
+        isIos = navigator.userAgent.match(/iphone|ipod|ipad/gi),
+        dpr = Math.min(win.devicePixelRatio, 3);
+    scale = 1 / dpr,
+        resizeEvent = 'orientationchange' in window ? 'orientationchange' : 'resize';
+    docEle.dataset.dpr = dpr;
+
+    var metaEle = doc.createElement('meta');
+    metaEle.name = 'viewport';
+    metaEle.content = 'initial-scale=' + scale + ',maximum-scale=' + scale;
+    docEle.firstElementChild.appendChild(metaEle);
+
+    var recalCulate = function()  {
+        var width = docEle.clientWidth;
+        if (width / dpr > 640) {
+            width = 640 * dpr;
+        }
+        docEle.style.fontSize =100 * (width / 750) + 'px';
+    };
+
+    recalCulate()
     if (!doc.addEventListener) return;
-    win.addEventListener(resizeEvt, recalc, false);
-    doc.addEventListener('DOMContentLoaded', recalc, false);
-})
-(document, window);
-$(document).ready(function(){
+    win.addEventListener(resizeEvent, recalCulate, false);
+})(document, window);
+$(document).ready(function () {
     // 点击领取
-    $('#allbtn1').click(function(){
-        showyn('#allbtn1','btnAll')
+    $('#allbtn1').click(function () {
+        showyn('#allbtn1', 'btnAll')
         $('#backChange').removeClass("backTwo").addClass("backOne");
     })
     // 点击第二个领取
-    $('#allbtn2').click(function(){
-        showyn('#allbtn2','btnAll')
+    $('#allbtn2').click(function () {
+        showyn('#allbtn2', 'btnAll')
         $('#backChange').removeClass("backOne").addClass("backTwo");
     })
 
     // 确定按钮
-    $('.popBtn1').click(function(){
+    $('.popBtn1').click(function () {
         let class_obj = $(this).parent('.popBtn').parent('.popText').siblings('div').prop('class')
-        popClose('.pop','.cover')
+        popClose('.pop', '.cover')
         if (class_obj == 'backOne') {
             $('#allbtn1').removeClass("btn").addClass("btnAll");
             $('#allbtn2').removeClass("btn").addClass("btnAll");
@@ -36,16 +62,16 @@ $(document).ready(function(){
         }
     });
     // 隐藏
-    $('.close').click(function(){
+    $('.close').click(function () {
         popClose('.pop')
     });
 
-    $('.popBtn2').click(function(){
+    $('.popBtn2').click(function () {
         popClose('.pop')
     });
 })
 // 点击按钮是否展示弹窗
-function showyn (obj,class_str) {
+function showyn(obj, class_str) {
     if (!$(obj).prop('class').includes(class_str)) {
         popShow('.pop');
     }
@@ -53,23 +79,23 @@ function showyn (obj,class_str) {
 
 
 // 显示
-function popShow (a1,a2) {
+function popShow(a1, a2) {
     $(a1).show();
     showMask();
 }
-function popClose (p1,p2) {
+function popClose(p1, p2) {
     $(p1).hide();
     hideMask();
 }
 //显示遮罩层
-function showMask(){
-    $("#mask").css("height",$(document).height());
-    $("#mask").css("width",$(document).width());
+function showMask() {
+    $("#mask").css("height", $(document).height());
+    $("#mask").css("width", $(document).width());
     $("#mask").show();
-    $('body').css('position','fixed');
+    $('body').css('position', 'fixed');
 }
 //隐藏遮罩层
-function hideMask(){
+function hideMask() {
     $("#mask").hide();
-    $('body').css('position','unset');
+    $('body').css('position', 'unset');
 }
